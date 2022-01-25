6d5182248c7caf10506b6aa969cca6fb27a74c0f
Azamat Shogen <baki47hanma@gmail.com>
2022-01-24 23:13:59 -0600

    navbar work

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
          </main>
     </div>
   );
-}
+};
 
 
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import "./header2.css"
+import "./header2.css";
+import { GiWolfHowl,  } from "react-icons/gi";
+import { MdLightMode } from "react-icons/md";
+
 import Navbar from "../navbar/Navbar";
 
 
@@ -11,9 +14,9 @@ const Header = () => {
         <header className="header">
 
 
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark nav-div">
                 <h3>
-                    <a href="/" className="link">AZ</a>
+                    <a href="/" className="az">AZ.</a>
                 </h3>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,31 +25,41 @@ const Header = () => {
             <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item active">
-                        <a className="nav-link" href="/">ABOUT <span className="sr-only">(current)</span></a>
+                        <a className="nav-link  selected" href="/">ABOUT</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">PROJECTS</a>
+                        <a className="nav-link nav_link" href="#">PROJECTS</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">SKILLS</a>
+                        <a className="nav-link " href="#">SKILLS</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link disabled" href="#">CONTACTS</a>
+                        <a className="nav-link" href="#">CONTACTS</a>
                     </li>
 
                 </ul>
                 <div className="nav-item">
-                    <span style={{color: "blueviolet", margin: "20px"}} >logo</span>
+
+                    <span className="icon-span" style={{color: "orange", margin: "50px 10px"}} >
+                         <GiWolfHowl
+                          color="white"
+                          size="2.5em"
+                          className="wolf"
+                         />
+                        {/*<MdLightMode*/}
+                        {/*    color="white"*/}
+                        {/*    size="2.5em"*/}
+                        {/*    className="wolf"*/}
+                        {/*/>*/}
+                    </span>
                 </div>
 
             </div>
 
         </nav>
 
-
-            {/*<Navbar />*/}
         </header>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
