05660b593a6291e90b80a95ff7605b225eda6725
Azamat Shogen <baki47hanma@gmail.com>
2022-01-26 21:21:57 -0600

    added skills component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import Header from "./components/header/Header";
 import About from "./components/about/About";
 import Projects from "./components/projects/Projects";
+import Skills from "./components/skills/Skills";
+
 
 function App() {
   return (
@@ -10,7 +12,7 @@ function App() {
          <Header />
          <main>
              <About />
-             <hr />
+             <Skills />
              <Projects />
              
          </main>
