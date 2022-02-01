38c2f249d3ed6f12dbad5ac524fa7b3044157602
Azamat Shogen <baki47hanma@gmail.com>
2022-02-01 16:10:58 -0600

    light color work

---
+++ src/components/top/Top.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {BsFillArrowUpSquareFill} from "react-icons/bs"
+
+const Top = () => {
+
+    return (
+     <BsFillArrowUpSquareFill />
+    )
+}
+
+export default Top;
