ecba79951f733d5ded1f17f4e03b57d11912ab82
Azamat Shogen <baki47hanma@gmail.com>
2022-02-05 17:26:52 -0600

    footer work completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,31 @@
 import React from "react";
+import {BsGithub, BsLinkedin} from "react-icons/bs"
+import {MdOutlineAlternateEmail} from "react-icons/md";
+import {FaCopyright} from "react-icons/fa"
+import {footer} from "../../data";
 import "./footer.css";
 
 const Footer = () => {
+    const {email, git, linkedin} = footer;
 
     return (
        <footer className="footer">
           <div className="footer-div">
-              I am a footer
+             <div className="footer-icons">
+                 <a href={`mailto:${email}`}>
+                 <MdOutlineAlternateEmail className="footer-icon e_mail" />
+                 </a>
+                 <a href={git}>
+                 <BsGithub className="footer-icon" />
+                 </a>
+                 <a href={linkedin}>
+                 <BsLinkedin className="footer-icon"/>
+                 </a>
+             </div>
+              <div className="copyright">
+                  <FaCopyright className="copyright-icon" />
+                  <span className="copy-text">Created by Azamat Shogen</span>
+              </div>
           </div>
        </footer>
     )
